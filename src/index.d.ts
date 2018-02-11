174f44836d4143f6db818af4d49150e0d1a0a76f
drag13 <ruban.vitaliy@gmail.com>
2018-02-11 13:45:30 +0200

    dts reworked

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,22 @@
-declare module '@drag13/when-do' {
+// Type definitions for WhenDo
+// Project: https://github.com/Drag13/WhenDo
+// Definitions by: Drag13 https://github.com/Drag13
 
-    export interface whenDo<TResult> {
-        (predicate: (() => Boolean) | Boolean, trueAction?: handler<TResult>, elseAction?: handler<TResult>): handler<TResult>
-    }
+/* =================== USAGE ===================
 
+     import * as whenDo from '@drag13/when-do';
+
+ =============================================== */
+
+/**
+* Compose new function from predicate, trueAction and elseAction. 
+*/
+declare function wd<TResult>(predicate: (() => Boolean) | Boolean, trueAction?: wd.handler<TResult>, elseAction?: wd.handler<TResult>): wd.handler<TResult>;
+
+declare namespace wd {
     interface handler<TResult> {
         (...args: any[]): TResult
     }
 }
+
+export = wd;
